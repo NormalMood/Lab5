c4bf97ef8060e93d55fd28f7336b1a175f8d0565
NormalMood <littlebigdiamond@gmail.com>
2024-03-13 17:38:25 +0300

    feat: add print matrix func

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <mpi.h>
+
+void printMatrix(const std::vector<std::vector<double>>& matrix) {
+    for (const auto& row : matrix) {
+        for (double val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    int n = 3; // Размерность СЛАУ
+    std::vector<std::vector<double>> A(n, std::vector<double>(n)); // Матрица коэффициентов
+    std::vector<double> b(n); // Вектор свободных членов
+
+    return 0;
+}
 
